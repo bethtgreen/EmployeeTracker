f97f68537cf890dc98dbc0ba18062f359b070aaa
Bethany Green <b.gree10@gail.com>
2021-01-27 20:31:39 -0600

    beginning the start function and the .then function

--- db/index.js
+++ db/index.js
@@ -12,7 +12,7 @@ var connection = mysql.createConnection({
   user: "root",
 
   // Your password
-  password: "",
+  password: "rootroot",
   database: "employees"
 });
 // connect to the mysql server and sql database
@@ -21,3 +21,27 @@ connection.connect(function(err) {
     // run the start function after the connection is made to prompt the user
     start();
   });
+  //function to ask user if they would like to add anything new
+  function start() {
+    inquirer
+      .prompt([
+       { name: "databasePrompt",
+        type: "list",
+        message: "Would you like to ADD to, VIEW or UPDATE the database",
+        choices: ["ADD", "VIEW", "UPDATE", "EXIT"]
+       }
+    ])
+    .then(function(answer){
+        switch (answer.databasePrompt) {
+            case "ADD":
+                // return addDatabase()
+            case "VIEW":
+                // return
+            case  "UPDATE":
+                // return
+            case "EXIT":
+                return connection.end();
+
+        }
+    })
+  }
